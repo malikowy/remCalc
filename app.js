961ee881e2ab53ba806af1db35817c30ae134a31
Malikowy <gabriel.ignatiuk@gmail.com>
2017-02-20 17:20:47 +0100

    changed var to let

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var remCalc = document.getElementById("remCalc");
+let remCalc = document.getElementById("remCalc");
 remCalc = addEventListener("click", calculator, false)
 
-var pxCalc = document.getElementById("pxCalc");
+let pxCalc = document.getElementById("pxCalc");
 pxCalc = addEventListener("click", calculator, false);
 
 function calculator() {
-    var baseDocPixelPx = document.getElementById("baseDocPixelPx").value;
-    var baseDocPixelRem = document.getElementById("baseDocPixelRem").value;
-    var userPixelSize = document.getElementById("userPixelSize").value;
-    var userRemSize = document.getElementById("userRemSize").value;
+    let baseDocPixelPx = document.getElementById("baseDocPixelPx").value;
+    let baseDocPixelRem = document.getElementById("baseDocPixelRem").value;
+    let userPixelSize = document.getElementById("userPixelSize").value;
+    let userRemSize = document.getElementById("userRemSize").value;
     if (userPixelSize > 0 && baseDocPixelRem > 0) {
-        var rems = userPixelSize / baseDocPixelRem;
+        let rems = userPixelSize / baseDocPixelRem;
         remsDecim = +rems.toFixed(4);
         document.getElementById("remCalculator").innerHTML = remsDecim + " rem";
     } else {
         document.getElementById("remCalculator").innerHTML = "Nie podano wartości do obliczeń PX->REM";
     }
     if (baseDocPixelPx > 0 && userRemSize > 0) {
-        var pixels = userRemSize * baseDocPixelPx;
+        let pixels = userRemSize * baseDocPixelPx;
         document.getElementById("pxCalculator").innerHTML = pixels + " pixels";
     } else {
         document.getElementById("pxCalculator").innerHTML = "Nie podano wartości do obliczeń REM->PX";
